c322d483408551fb2f4a666b691d21eb7a065ca0
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-08-28 20:40:57 +0530

    Update step1-open_Image.py

--- step1-open_Image.py
+++ step1-open_Image.py
@@ -4,3 +4,4 @@ img = cv2.imread('/Users/ashukumar/Downloads/m.jpg',1)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite('messigray.png',img) #save images first argument nameof the saved file second argument the data to be saved
